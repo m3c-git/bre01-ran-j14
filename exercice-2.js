6584f0a309ca0455ceda853008db49a2c82d3a5c
m3c-git <ayyoubentreprise@gmail.com>
2023-11-02 11:27:45 +0000

    adding update ex1 ex2

--- exercice-2.js
+++ exercice-2.js
@@ -1,15 +1,16 @@
 window.addEventListener("DOMContentLoaded", function(){
 
-    let el = document.querySelector("section");
     let newh2 = document.createElement("h2");
     let newH2Text = document.createTextNode("Le titre de la deuxième section");
     let newP = document.createElement("p");
     let newPText = document.createTextNode("Le paragraphe de la deuxième section");
-    let el2 = document.querySelector("h2");
+    let el2 = document.querySelector("body :nth-child(3)");
+    let el3 = document.querySelector("body :nth-child(3)");
 
-    
-    el.appendChild(newh2).appendChild(newH2Text);
 
+    el2.appendChild(newh2).appendChild(newH2Text)
+    el3.appendChild(newP).appendChild(newPText)
+   
 
     
     
---
+++ exercice-3.js
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("p").innerHTML = <p>Pour accéder à Google cliquez<a href="https://google.com/">ici</a><p>
+    
+});
